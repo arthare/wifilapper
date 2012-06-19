71af7ec6dbc73ee46124789636b9cc489c6fe8d4
art <art@wifilapperhome>
2012-06-19 17:48:26 -0400

    adding gpl message

--- TestApp/src/com/artsoft/wifilapper/SummaryActivity.java
+++ TestApp/src/com/artsoft/wifilapper/SummaryActivity.java
@@ -1,3 +1,19 @@
+// Copyright 2011-2012, Art Hare
+// This file is part of WifiLapper.
+
+//WifiLapper is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//WifiLapper is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with WifiLapper.  If not, see <http://www.gnu.org/licenses/>.
+
 package com.artsoft.wifilapper;
 
 import java.io.File;
