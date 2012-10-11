e4b819ee771bbbb41a4f3f0244026659fa89560a
art <art@wifilapperhome>
2012-10-11 15:30:52 -0400

    fixing db load issue.

--- PitsideConsole/PitsideConsole/SQLiteLapDB.cpp
+++ PitsideConsole/PitsideConsole/SQLiteLapDB.cpp
@@ -170,7 +170,7 @@ static LPCTSTR CREATE_RACE_SQL_V22 =  L"create table races (	_id integer primary
 																						L"vx3 real," \
 																						L"vy3 real," \
                                             L"p2p integer not null default 0," \
-                                            L" finishcount integer not null default 1)";
+                                            L"finishcount integer not null default 1)";
 	
 static LPCTSTR CREATE_LAPS_SQL = L"create table laps " \
 												L"(_id integer primary key asc autoincrement, " \
