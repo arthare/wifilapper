c1f03fd8abab8b6de3a846c05bf56b79c1c5732d
art <art@wifilapperhome>
2012-10-24 19:08:14 -0400

    Adding stuff to artvector.

--- PitsideConsole/ArtLib/ArtVector.cpp
+++ PitsideConsole/ArtLib/ArtVector.cpp
@@ -9,3 +9,10 @@ Vector2D V2D(double x, double y)
   v.m_v[1] = y;
   return v;
 }
+Vector2D V2D(const Vector2D& pos, double dAng, double dLen)
+{
+  Vector2D v;
+  v.m_v[0] = pos.m_v[0] + cos(dAng)*dLen;
+  v.m_v[1] = pos.m_v[1] + sin(dAng)*dLen;
+  return v;
+}
