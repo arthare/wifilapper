0abd6ffb385e0cfd0da82cc7b847e1ab07979d78
KDJones2 <KDJones2@yahoo.com>
2013-01-05 14:55:06 -0700

    Fixed distance scaling :)

--- PitsideConsole/PitsideConsole/LapData.cpp
+++ PitsideConsole/PitsideConsole/LapData.cpp
@@ -693,12 +693,12 @@ void CExtendedLap::ComputeLapData(const vector<TimePoint2D>& lstPoints, CExtende
 			// default 4 sig figs reflects typical 0.3% accuracy of spherical model
 			double dLat, dLon, R, lat1, lat2, lon1, lon2;
 			R = rad;
-			lat1 = p.flY;
-			lon1 = p.flX;
-			lat2 = ptLast.flY;
-			lon2 = ptLast.flX;
-			dLat = lat2 - lat1;
-			dLon = lon2 - lon1;
+			lat1 = p.flY * 0.0174532925199433;	// Convert from degrees to radians
+			lon1 = p.flX * 0.0174532925199433;
+			lat2 = ptLast.flY * 0.0174532925199433;
+			lon2 = ptLast.flX * 0.0174532925199433;
+			dLat = (lat2 - lat1);
+			dLon = (lon2 - lon1);
 			double a = sin(dLat/2) * sin(dLat/2) + cos(lat1) * cos(lat2) * sin(dLon/2) * sin(dLon/2);
 			double c = 2 * atan2(sqrt(a), sqrt(1-a));
 			const double d = R * c * 1000;	// Return the distance in meters
